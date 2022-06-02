68f4b60c5664b14dda6a9fe5b882338661c52e6b
Sunjay Armstead <connect@sunjayarmstead.local>
2022-06-02 09:22:33 -0400

    WIP. Create dynamic article route for CNN.

---
+++ pages/news/cnn/[...article].js
@@ -0,0 +1,34 @@
+import { useRouter } from 'next/router';
+import getCNNLinks from '../../../utils/api/cnn-links';
+
+const Article = (props) => {
+  const router = useRouter();
+  console.log('Context:', props)
+  const { article } = router.query;
+
+  return <p>Article Rendered: {article}</p>
+}
+
+export const getStaticPaths = async () => {
+  const allLinks = await getCNNLinks('https://lite.cnn.com');
+  const ids = allLinks.map(link => {
+    return link.id
+  })
+  
+  return {
+    paths: [
+      { params: { article: ids } }
+    ],
+    fallback: true
+  };
+}
+
+export const getStaticProps = (context) => {
+  // TO DO: create function to get single article from CNN. Put the function here to fetch.
+  const endpoint = `https://lite.cnn.com/en/article/${context.params.article[0]}`
+  return {
+    props: { endpoint }
+  }
+}
+
+export default Article;
