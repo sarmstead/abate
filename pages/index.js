ad7f85a578a65ed1ebb5d9f4453d1373ef3d0624
Sunjay Armstead <connect@sunjayarmstead.local>
2022-06-03 14:36:30 -0400

    Move fetch to client side and add error and loading states

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,45 @@
 import Link from 'next/link';
+import { useEffect, useState } from 'react'
 
-export default function Home({ data }) {
+export default function Home() {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
+  
+  useEffect(() => {
+    async function fetchData() {
+      setIsLoading(true)
+
+      try {
+        const res = await fetch('/api/news/cnn/links?num=10');
+        const responseData = await res.json();
+        setData(responseData)
+      }
+
+      catch (error){
+        console.error(error)
+        setError(error.toString())
+      }
+      setIsLoading(false)
+    }
+    
+    fetchData()
+    
+  }, [])
+  
+  if(error) {
+    return (
+      <p>Yikes! We came across the following error:
+        <br />
+        <span className='italic text-red-500'>{error}</span>
+      </p>
+    )
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div className='grid max-w-screen-2xl m-auto p-14 lg:p-24'>
       <h1 className='mb-10'>
@@ -16,15 +55,4 @@ export default function Home({ data }) {
       }
     </div>
   )
-}
-
-export const getStaticProps = async () => {
-  const res = await fetch('/api/news/cnn/links?num=10');
-  const data = await res.json();
-  
-  return { 
-    props: {data},
-    revalidate: 3600
-  
-  }
 }
