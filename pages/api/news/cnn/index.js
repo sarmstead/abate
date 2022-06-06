61ce799f56566790650bdd12f3a114f49c989f48
Sunjay Armstead <connect@sunjayarmstead.local>
2022-06-05 21:00:19 -0400

    Refactor using cnn article util

--- pages/api/news/cnn/index.js
+++ pages/api/news/cnn/index.js
@@ -1,6 +1,5 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
 import getCNNLinks from '../../../../utils/api/cnn-links';
+import getCNNArticle from '../../../../utils/api/cnn-article';
 
 const cnnUrl = 'https://lite.cnn.com';
 
@@ -14,35 +13,7 @@ const handler = async (req, res) => {
 
         for (let i = 0; i < pathArray.length; i++) {
             responses.push(
-                await axios(pathArray[i])
-                    .then(pageData => {
-                        let responseObject = {};
-                        let contentArray = [];
-                        const html = pageData.data;
-                        const $ = cheerio.load(html)
-                        responseObject.title = $('h2').text();
-                        responseObject.byline = $('#byline').text();
-
-                        let startDateIndex;
-                        let endDateIndex;
-                        for (let i = 0; i < html.length; i++) {
-                            if (html.charAt(i) === '>' && html.charAt(i+1) === 'U' && html.charAt(i+2) === 'p' && html.charAt(i+3) === 'd') {
-                                startDateIndex = i + 1;
-                            }
-
-                            if (html.charAt(i) === 'M' && html.charAt(i+1) === 'T' && html.charAt(i+2) === '<') {
-                                endDateIndex = i + 2;
-                            }
-                        }
-                        responseObject.date = html.substring(startDateIndex, endDateIndex);
-
-                        responseObject.editorsNote = $('#editorsNote').text();
-                        $('.afe4286c p').each(function() {
-                            contentArray.push($(this).text())
-                        })
-                        responseObject.content = contentArray;
-                        responses.push(responseObject)
-                    })
+                await getCNNArticle(pathArray[i])
             )
         }
         
