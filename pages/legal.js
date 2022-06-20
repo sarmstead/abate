3cbb02183ddb01fb942bbcd4b23545e05f2daee9
Sunjay Armstead <connect@sunjayarmstead.local>
2022-06-20 17:29:40 -0400

    Add legal language

---
+++ pages/legal.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import Page from '../components/global/page';
+
+const Legal = () => {
+  return (
+    <Page>
+      <h1 className='font-bold dark:text-primary-teal text-4xl mb-4'>Terms of Use</h1>
+      <p>Abate is a project created by <Link href='https://sunjayarmstead.com/about/'><a target='_blank'>Sunjay Armstead</a></Link>.</p>
+      <p>Our service is free to use and all of our code is <Link href='https://github.com/sarmstead/abate'><a target='_blank'>open source</a></Link>.</p>
+      <p>The purpose of this project is to aggregate news from a variety of sources in one place. We are committed to the fidelity of original information and the proper citation of every source used. You will find bibliographic information, including links to the original posting, after each individual article. We only aggregate from text-only sources that already remove advertisements and other multimedia content.</p>
+      <p>You may contact us at <Link href='https://sunjayarmstead.com/contact/'><a target='_blank'>sunjayarmstead.com/contact</a></Link> if you have any questions about the terms of using this application.</p>
+    </Page>
+  )
+}
+
+export default Legal;
