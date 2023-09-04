81cf3323d57ec93e671761271688795abbe72278
Sunjay Armstead <connect@sunjayarmstead.local>
2023-09-04 18:20:44 -0400

    Set footer year to dynamic JavaScript function

--- components/global/footer.js
+++ components/global/footer.js
@@ -2,10 +2,12 @@ import Link from 'next/link';
 import Logo from './logo';
 
 const Footer = props => {
+  const today = new Date(Date.now())
+
   return (
     <footer className='max-w-4xl border-t border-primary-teal mx-auto mt-20 flex flex-col justify-center'>
       <Link href='/'><span className='cursor-pointer mx-auto mt-8 mb-10'><Logo type='full' {...props} /></span></Link>
-      <small className='text-xs tracking-wide text-center font-sans text-black dark:text-white uppercase font-semibold'>Copyright © 2022 Sunjay Armstead. All rights reserved. <Link href='/legal'><a className='underline decoration-primary-teal'>Terms of Use.</a></Link></small>
+      <small className='text-xs tracking-wide text-center font-sans text-black dark:text-white uppercase font-semibold'>Copyright © {today.getFullYear()} Sunjay Armstead. All rights reserved. <Link href='/legal'><a className='underline decoration-primary-teal'>Terms of Use.</a></Link></small>
     </footer>
   )
 }
